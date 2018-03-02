4bd371cdd48d99c382c2df81be918f0a482682ff
Marivit <maria.vitali@correo.unimet.edu.ve>
2018-03-02 18:56:10 -0400

    ..

---
+++ Proyecto MatiAssembler/src/app/Components/profile-gerente/profile-gerente.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'q';
+import { Http, Headers } from '@angular/http';
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Vehiculo } from '../vehiculo/vehiculo'; 
+
+@Component({
+  selector: 'app-profile-gerente',
+  templateUrl: './profile-gerente.component.html',
+  styleUrls: ['./profile-gerente.component.css']
+})
+export class ProfileGerenteComponent implements OnInit {
+  
+  user;
+
+  constructor(private http:Http,
+              private validateService: ValidateService, 
+              private authService: AuthService,
+              private router: Router, 
+              private location: Location) { 
+    
+  }
+
+  ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem("user")); 
+    //console.log(this.user); 
+  }
+
+  logout() {
+    this.authService.logout(); 
+    this.router.navigate(['login']); 
+  }
+
+  home() {
+    this.router.navigate(['']);
+  }
+
+}

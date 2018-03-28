3e2430efdaba1eead80113b60ce302a98ad83cef
Luciano Pinedo <lucianopinedo@gmail.com>
2018-03-28 12:01:41 -0400

    asd
    asd

---
+++ Proyecto MatiAssembler/src/app/Components/modificar-repuesto/modificar-repuesto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-repuesto',
+  templateUrl: './modificar-repuesto.component.html',
+  styleUrls: ['./modificar-repuesto.component.css']
+})
+export class ModificarRepuestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
